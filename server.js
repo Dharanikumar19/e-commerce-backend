3c1141228629269f9e9a20ea1e2905a8c3c9730b
Dharanikumar19 <dharanikumarmuthusamy@gmail.com>
2022-03-25 17:47:25 +0530

    express json

--- server.js
+++ server.js
@@ -2,18 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({limit :"50mb", extended : true}));
+app.use(express.urlencoded({limit : "50mb", extended : true}))
+
 app.use(cors());
 app.use(cookieParser());
-
-
-app.use(fileUpload({
-    useTempFiles : true
-}))
 
 
 //Routes
